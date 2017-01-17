e42d927a15756dc16e91bc2446ca5b68fd3135c0
Renee Lightner <Renee.Lightner@wsj.com>
2017-01-16 21:25:56 -0500

    viz class started

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 $(document).ready(function () {
 
@@ -9,18 +13,66 @@ $(document).ready(function () {
 		return $.getJSON(url);
 	}
 
+	//Class declaration
+
+	var MovieViz = function () {
+		function MovieViz(opts) {
+			_classCallCheck(this, MovieViz);
+
+			this.data = opts.data;
+			this.element = opts.element;
+			this.xDomain = opts.xDomain;
+			this.chartHeight = opts.chartHeight;
+			this.genres = opts.genres;
+
+			this.draw();
+		}
+
+		_createClass(MovieViz, [{
+			key: 'draw',
+			value: function draw() {
+
+				this.chartWidth = $(this.element).width();
+
+				// define width, height with the margin
+				this.margin = {
+					top: 5,
+					right: 20,
+					bottom: 20,
+					left: 20
+				};
+				this.width = this.chartWidth - this.margin.left - this.margin.right;
+				this.height = this.chartHeight - this.margin.top - this.margin.bottom;
+
+				// set up parent element and SVG
+				this.element.innerHTML = '';
+				var svg = d3.select(this.element).append('svg');
+				svg.attr('width', this.width + this.margin.left + this.margin.right);
+				svg.attr('height', this.height + this.margin.top + this.margin.bottom);
+
+				// we'll actually be appending to a <g class='baseGroup'> element
+				this.baseGroup = svg.append('g').attr("class", "baseGroup").attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+			}
+		}]);
+
+		return MovieViz;
+	}();
+
 	getData('./admin/movies.json')
 	//then() method takes to params: callback functions for the success and failure cases of the Promise
 	.then(function (data) {
 		//add the total number of movies
 		$(".dek span").html(data.movies.length);
 
-		/*	        var movieViz = new LineChart({
-  				element: document.querySelector('#viz'),
-  				data: data2.tier1.tiersdata,
-  			    yDomain: [1,3.5], 
-  			    lineClass: 'tierPath'
-  			}); */
+		var testMovieViz = new MovieViz({
+			element: document.querySelector('#viz'),
+			data: data.movies,
+			xDomain: [1, 3.5],
+			chartHeight: 400,
+			genres: 'all'
+		});
+
+		console.log(data);
 	});
 });
 
--- js/src/app.es6
+++ js/src/app.es6
@@ -7,18 +7,62 @@ $( document ).ready(() => {
       	return $.getJSON(url); 
     }
 
-    getData('./admin/movies.json')
+    //Class declaration
+	class MovieViz {
+
+	    constructor(opts) {
+	        this.data = opts.data;
+	        this.element = opts.element;
+	        this.xDomain = opts.xDomain;
+	        this.chartHeight = opts.chartHeight;
+	        this.genres = opts.genres;
+
+	        this.draw();
+	    }
+
+	    draw () {
+
+	        this.chartWidth = $(this.element).width();
+	      
+	        // define width, height with the margin
+	        this.margin = {
+	            top: 5,
+	            right: 20,
+	            bottom: 20,
+	            left: 20
+	        };
+	        this.width = this.chartWidth - this.margin.left - this.margin.right;
+	        this.height = this.chartHeight - this.margin.top - this.margin.bottom;
+	        
+	        // set up parent element and SVG
+	        this.element.innerHTML = '';
+	        var svg = d3.select(this.element).append('svg');
+	        svg.attr('width',  this.width + this.margin.left + this.margin.right);
+	        svg.attr('height', this.height + this.margin.top + this.margin.bottom);
+
+	        // we'll actually be appending to a <g class='baseGroup'> element
+	        this.baseGroup = svg
+	          .append('g')
+	          .attr("class", "baseGroup")
+	          .attr('transform','translate('+this.margin.left+','+this.margin.top+')');
+		}
+	}
+
+	getData('./admin/movies.json')
     	//then() method takes to params: callback functions for the success and failure cases of the Promise
       	.then(function(data) {
 	        //add the total number of movies
 	        $(".dek span").html(data.movies.length);
 
-/*	        var movieViz = new LineChart({
+	        const testMovieViz = new MovieViz({
 				element: document.querySelector('#viz'),
-				data: data2.tier1.tiersdata,
-			    yDomain: [1,3.5], 
-			    lineClass: 'tierPath'
-			}); */
+				data: data.movies,
+			    xDomain: [1,3.5], 
+			    chartHeight: 400,
+			    genres: 'all'
+			}); 
+
+			console.log(data);
       	});
 });
 
